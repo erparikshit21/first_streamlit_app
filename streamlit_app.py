ffb06e2177ab31c47ec021e6b99013f4ebe4fcc2
erparikshit21 <145456831+erparikshit21@users.noreply.github.com>
2023-09-20 16:24:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,6 @@ streamlit.text(fruityvice_response.json())
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.text(fruityvice_normalized)
+#streamlit.text(fruityvice_normalized)
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
